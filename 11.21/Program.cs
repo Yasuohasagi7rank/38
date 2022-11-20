5935a5e355c03f756712c564174a021b952774b1
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:33:08 +0300

    Add files via upload

---
+++ 11.21/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _11._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            b = 237;
+            c = b / 100;
+            d = b % 100;
+            a = d * 10 + c;
+            Console.WriteLine(a);
+        }
+    }
+}
